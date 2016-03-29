e224b177d932e867e666a3edfb3d7de6d62499f2
anisha jauhari <anishajauhari@MacBook-Air.local>
2016-03-29 21:28:37 +0530

    precision and recall

--- main.m
+++ main.m
@@ -2,3 +2,5 @@ theta = trainClassifier();
 disp(theta);
 [Jcv,predacc] = crossValidateClassifier(theta);
 predacc = predacc*100
+[Jcv,precision , recall,F1Score]= precisionandrecall(theta)
+
---
+++ precisionandrecall.m
@@ -0,0 +1,33 @@
+% calculate precision and recall
+function [J,precision , recall , F1Score] = precisionandrecall(theta)
+
+  data =csvread('mycsvcv.csv');
+  X= data(:,[1:1000]);
+  y= data( : ,1001);
+  [m, n] = size(X);
+  X = [ones(m, 1) X];
+  [J,predY]=costfunction_test(theta,X,y);
+  tp=0   %true positive
+  tn=0   %true negative
+  fp=0   %false positive
+  fn=0   %false negative
+  for i = 1:m
+    if y(i)==predY(i)
+        if y(i)==1
+            tp=tp+1;
+        else
+            tn=tn+1;
+        end
+    else 
+        if y(i)==1
+            fn=fn+1;
+        else 
+            fp=fp+1;
+        end
+    end
+  end
+  precision = tp / (tp +fp);
+  recall = tp / (tp +fn);
+  F1Score = (2 * precision * recall) / (precision+recall);
+
+end
