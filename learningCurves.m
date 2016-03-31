4a92a23a2fcf4442a887eda1d8e5960151d07c37
anisha jauhari <anishajauhari@MacBook-Air.local>
2016-03-31 16:26:35 +0530

    learning curves

--- learningCurves.m
+++ learningCurves.m
@@ -3,7 +3,7 @@ function [error_train , error_cv , m] = learningCurves(X ,y ,Xcv,ycv)
 %error_train = zeros(m, 1);
 %error_cv   = zeros(m, 1);
 error_train = zeros(2, 1);
-verror_cv   = zeros(2, 1);
+error_cv   = zeros(2, 1);
 
 for i=1:2
 [theta] = train(X(1:i,:),y(1:i));
