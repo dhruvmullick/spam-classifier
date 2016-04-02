faeb3abfc6aa5dcce16399f7129faea16c521504
Dhruv Mullick <dmullick95@gmail.com>
2016-04-02 16:16:55 +0530

    Correction

--- main.m
+++ main.m
@@ -8,34 +8,34 @@ y= data( : ,1001);
 X = [ones(m, 1) X];
 
 %read the cv set
-data =csvread('mycsvcv.csv');
-Xcv= data(:,[1:1000]);
-ycv= data( : ,1001);
-[m1, n1] = size(Xcv);
-Xcv = [ones(m1, 1) Xcv];
+% data =csvread('mycsvcv.csv');
+% Xcv= data(:,[1:1000]);
+% ycv= data( : ,1001);
+% [m1, n1] = size(Xcv);
+% Xcv = [ones(m1, 1) Xcv];
 
 %read the test set
 
-% data =csvread('mycsvtest.csv');
-% Xt= data(:,[1:100]);
-% yt= data( : ,1001);
-% [m1, n1] = size(Xt);
-% Xt = [ones(m1, 1) Xt];
+data =csvread('mycsvtest.csv');
+Xt= data(:,[1:100]);
+yt= data( : ,1001);
+[m1, n1] = size(Xt);
+Xt = [ones(m1, 1) Xt];
 
 
 
 %predict value of theta and find the cost of CV/test
 [theta] = trainClassifier(X,y,lambda);
-[Jcv,predacc] = cv_testPrediction(Xcv,ycv,theta,lambda);
+% [Jcv,predacc] = cv_testPrediction(Xcv,ycv,theta,lambda);
+[Jt,predacc] = cv_testPrediction(Xt,yt,theta,lambda);
 
-% [Jcv,predacc] = cv_testPrediction(Xt,yt,theta,lambda);
 predacc = predacc*100
 %[precision , recall,F1Score]= precisionandrecall(theta , Xcv ,ycv,lambda)
-[error_train,error_cv ,m]=learningCurves(X,y,Xcv,ycv,lambda);
+% [error_train,error_cv ,m]=learningCurves(X,y,Xcv,ycv,lambda);
 %
-plot(1:9, error_train, 1:9, error_cv);
-%plot(1:m, error_train, 1:m, error_cv);
-title('Learning curve for logistic regression')
-legend('Train', 'Cross Validation')
-xlabel('Number of training examples')
-ylabel('Error')
+% plot(1:9, error_train, 1:9, error_cv);
+% %plot(1:m, error_train, 1:m, error_cv);
+% title('Learning curve for logistic regression')
+% legend('Train', 'Cross Validation')
+% xlabel('Number of training examples')
+% ylabel('Error')
